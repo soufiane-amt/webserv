e4bab623a6058832feb6b76eb7ac413bf50346bf
Soufiane Amajat <samajat@e2r10p1.1337.ma>
2023-03-16 16:26:47 +0100

    good

--- test/parsing_test/pars.cpp
+++ test/parsing_test/pars.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:21:57 by samajat           #+#    #+#             */
-/*   Updated: 2023/03/16 16:25:01 by samajat          ###   ########.fr       */
+/*   Updated: 2023/03/16 16:26:44 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,29 +73,11 @@ class utility
 };
 
 
-typedef  struct clientRequest
-{
-    typedef    std::string                          method_t;
-    typedef    std::string                          uri_t;
-    typedef    std::string                          protocolVersion_t;
-    typedef    std::map <std::string, std::string>  general_header;
-
-    method_t                                        method;
-    uri_t                                           uri;
-    protocolVersion_t                               protocol;
-    general_header                                  g_header;
-
-    general_header::mapped_type& operator [] (general_header::key_type key)
-    {
-        return (g_header[key]);
-    }
-}   request_t;
-
-
 class clientRequestParser
 {
     private:
     typedef    std::vector<std::string>             tokens_t;
+    typedef    std::map <std::string, std::string>  request_t;
 
 
     tokens_t                    tokens;

47b3e1b9ba965c60bb5e0738c58714c03b86bc1a
Soufiane Amajat <samajat@e1r4p15.1337.ma>
2023-03-19 15:15:41 +0000

    status codes defined as macros

--- includes/Utility/macros.hpp
+++ includes/Utility/macros.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 15:04:50 by samajat           #+#    #+#             */
-/*   Updated: 2023/03/19 15:08:25 by samajat          ###   ########.fr       */
+/*   Updated: 2023/03/19 15:15:06 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,33 +46,59 @@
                             // Status codes :
 /* ************************************************************************** */
 
+#define OK "200 OK"
+#define CREATED "201 Created"
+#define ACCEPTED "202 Accepted"
+#define NO_CONTENT "204 No Content"
+#define PARTIAL_CONTENT "206 Partial Content"
 
-#define OK 200
-#define CREATED 201
-#define ACCEPTED 202
-#define NO_CONTENT 204
-#define PARTIAL_CONTENT 206
+#define MULTIPLE_CHOICES "300 Multiple Choices"
+#define MOVED_PERMANENTLY "301 Moved Permanently"
+#define FOUND "302 Found"
+#define SEE_OTHER "303 See Other"
+#define NOT_MODIFIED "304 Not Modified"
+#define TEMPORARY_REDIRECT "307 Temporary Redirect"
 
-#define MULTIPLE_CHOICES 300
-#define MOVED_PERMANENTLY 301
-#define FOUND 302
-#define SEE_OTHER 303
-#define NOT_MODIFIED 304
-#define TEMPORARY_REDIRECT 307
+#define BAD_REQUEST "400 Bad Request"
+#define UNAUTHORIZED "401 Unauthorized"
+#define FORBIDDEN "403 Forbidden"
+#define NOT_FOUND "404 Not Found"
+#define METHOD_NOT_ALLOWED "405 Method Not Allowed"
+#define REQUEST_TIMEOUT "408 Request Timeout"
 
-#define BAD_REQUEST 400
-#define UNAUTHORIZED 401
-#define FORBIDDEN 403
-#define NOT_FOUND 404
-#define METHOD_NOT_ALLOWED 405
-#define REQUEST_TIMEOUT 408
+#define INTERNAL_SERVER_ERROR "500 Internal Server Error"
+#define NOT_IMPLEMENTED "501 Not Implemented"
+#define BAD_GATEWAY "502 Bad Gateway"
+#define SERVICE_UNAVAILABLE "503 Service Unavailable"
+#define GATEWAY_TIMEOUT "504 Gateway Timeout"
+#define HTTP_VERSION_NOT_SUPPORTED "505 HTTP Version Not Supported"
 
-#define INTERNAL_SERVER_ERROR 500
-#define NOT_IMPLEMENTED 501
-#define BAD_GATEWAY 502
-#define SERVICE_UNAVAILABLE 503
-#define GATEWAY_TIMEOUT 504
-#define HTTP_VERSION_NOT_SUPPORTED 505
+// #define OK 200
+// #define CREATED 201
+// #define ACCEPTED 202
+// #define NO_CONTENT 204
+// #define PARTIAL_CONTENT 206
+
+// #define MULTIPLE_CHOICES 300
+// #define MOVED_PERMANENTLY 301
+// #define FOUND 302
+// #define SEE_OTHER 303
+// #define NOT_MODIFIED 304
+// #define TEMPORARY_REDIRECT 307
+
+// #define BAD_REQUEST 400
+// #define UNAUTHORIZED 401
+// #define FORBIDDEN 403
+// #define NOT_FOUND 404
+// #define METHOD_NOT_ALLOWED 405
+// #define REQUEST_TIMEOUT 408
+
+// #define INTERNAL_SERVER_ERROR 500
+// #define NOT_IMPLEMENTED 501
+// #define BAD_GATEWAY 502
+// #define SERVICE_UNAVAILABLE 503
+// #define GATEWAY_TIMEOUT 504
+// #define HTTP_VERSION_NOT_SUPPORTED 505
 
 
 

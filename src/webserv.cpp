738b1d83bbab22a2368fbc5362a4004bf95bfbf3
Soufiane Amajat <samajat@e1r8p1.1337.ma>
2023-03-20 17:51:07 +0000

    trim method implemented

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -6,15 +6,15 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:56:03 by samajat           #+#    #+#             */
-/*   Updated: 2023/03/20 17:00:48 by samajat          ###   ########.fr       */
+/*   Updated: 2023/03/20 17:51:07 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "requestParser.hpp"
 #include "errorManager.hpp"
 
-std::string msg= "GET  /images/fdsfs HTTP/1.1\r\n"
-            "host: 192.241.213.46:6880\r\n"
+std::string msg= "GET /images/fdsfs HTTP/1.1\r\n"
+            " host: 192.241.213.46:6880\r\n"
             "Upgrade-Insecure-Requests: 1\r\n"
             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
             "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8\r\n"

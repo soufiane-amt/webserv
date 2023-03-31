81062d2e822bdfefb84f9a0583a487126694352b
Soufiane Amajat <samajat@e2r11p1.1337.ma>
2023-03-31 16:27:50 +0000

    status codes added for return direcctive handling

--- src/Utility/utils.cpp
+++ src/Utility/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:44:52 by samajat           #+#    #+#             */
-/*   Updated: 2023/03/31 16:13:40 by samajat          ###   ########.fr       */
+/*   Updated: 2023/03/31 16:27:29 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,14 +120,87 @@ std::pair <status_code, std::string> get_status_code(const std::string& redirect
     std::vector<std::string> tokens = utility::split(redirector, " ");
     
     
-    if (tokens[0] == "200")
-        return std::make_pair(status_code::OK, "OK");
-    if (tokens[0] == "201")
-        return std::make_pair(status_code::CREATED, "Created");
-    if (tokens[0] == "202")
-        return std::make_pair(status_code::ACCEPTED, "Accepted");
-    if (tokens[0] == "204")
-        return std::make_pair(status_code::NO_CONTENT, "No Content");
-    
+    if (token[0] == "100")
+        return (std::make_pair(status_code(CONTINUE), tokens[1]));   
+    if (token[0] == "101")
+        return (std::make_pair(status_code(SWITCHING_PROTOCOLS), tokens[1]));
+    if (token[0] == "200")
+        return (std::make_pair(status_code(OK), tokens[1]));
+    if (token[0] == "201")
+        return (std::make_pair(status_code(CREATED), tokens[1]));
+    if (token[0] == "202")
+        return (std::make_pair(status_code(ACCEPTED), tokens[1]));
+    if (token[0] == "203")
+        return (std::make_pair(status_code(NON_AUTHORITATIVE_INFORMATION), tokens[1]));
+    if (token[0] == "204")
+        return (std::make_pair(status_code(NO_CONTENT), tokens[1]));
+    if (token[0] == "205")
+        return (std::make_pair(status_code(RESET_CONTENT), tokens[1]));
+    if (token[0] == "206")
+        return (std::make_pair(status_code(PARTIAL_CONTENT), tokens[1]));
+    if (token[0] == "300")
+        return (std::make_pair(status_code(MULTIPLE_CHOICES), tokens[1]));
+    if (token[0] == "301")
+        return (std::make_pair(status_code(MOVED_PERMANENTLY), tokens[1]));
+    if (token[0] == "302")
+        return (std::make_pair(status_code(FOUND), tokens[1]));
+    if (token[0] == "303")
+        return (std::make_pair(status_code(SEE_OTHER), tokens[1]));
+    if (token[0] == "304")
+        return (std::make_pair(status_code(NOT_MODIFIED), tokens[1]));
+    if (token[0] == "305")
+        return (std::make_pair(status_code(USE_PROXY), tokens[1]));
+    if (token[0] == "307")
+        return (std::make_pair(status_code(TEMPORARY_REDIRECT), tokens[1]));
+    if (token[0] == "400")
+        return (std::make_pair(status_code(BAD_REQUEST), tokens[1]));
+    if (token[0] == "401")
+        return (std::make_pair(status_code(UNAUTHORIZED), tokens[1]));
+    if (token[0] == "402")
+        return (std::make_pair(status_code(PAYMENT_REQUIRED), tokens[1]));
+    if (token[0] == "403")
+        return (std::make_pair(status_code(FORBIDDEN), tokens[1]));
+    if (token[0] == "404")
+        return (std::make_pair(status_code(NOT_FOUND), tokens[1]));
+    if (token[0] == "405")
+        return (std::make_pair(status_code(METHOD_NOT_ALLOWED), tokens[1]));
+    if (token[0] == "406")
+        return (std::make_pair(status_code(NOT_ACCEPTABLE), tokens[1]));
+    if (token[0] == "407")
+        return (std::make_pair(status_code(PROXY_AUTHENTICATION_REQUIRED), tokens[1]));
+    if (token[0] == "408")
+        return (std::make_pair(status_code(REQUEST_TIMEOUT), tokens[1]));
+    if (token[0] == "409")
+        return (std::make_pair(status_code(CONFLICT), tokens[1]));
+    if (token[0] == "410")
+        return (std::make_pair(status_code(GONE), tokens[1]));
+    if (token[0] == "411")
+        return (std::make_pair(status_code(LENGTH_REQUIRED), tokens[1]));
+    if (token[0] == "412")
+        return (std::make_pair(status_code(PRECONDITION_FAILED), tokens[1]));
+    if (token[0] == "413")
+        return (std::make_pair(status_code(PAYLOAD_TOO_LARGE), tokens[1]));
+    if (token[0] == "414")
+        return (std::make_pair(status_code(URI_TOO_LONG), tokens[1]));
+    if (token[0] == "415")
+        return (std::make_pair(status_code(UNSUPPORTED_MEDIA_TYPE), tokens[1]));
+    if (token[0] == "416")
+        return (std::make_pair(status_code(RANGE_NOT_SATISFIABLE), tokens[1]));
+    if (token[0] == "417")
+        return (std::make_pair(status_code(EXPECTATION_FAILED), tokens[1]));
+    if (token[0] == "500")
+        return (std::make_pair(status_code(INTERNAL_SERVER_ERROR), tokens[1]));
+    if (token[0] == "501")
+        return (std::make_pair(status_code(NOT_IMPLEMENTED), tokens[1]));
+    if (token[0] == "502")
+        return (std::make_pair(status_code(BAD_GATEWAY), tokens[1]));
+    if (token[0] == "503")
+        return (std::make_pair(status_code(SERVICE_UNAVAILABLE), tokens[1]));
+    if (token[0] == "504")
+        return (std::make_pair(status_code(GATEWAY_TIMEOUT), tokens[1]));
+    if (token[0] == "505")
+        return (std::make_pair(status_code(HTTP_VERSION_NOT_SUPPORTED), tokens[1]));
+    return (std::make_pair(status_code(HTTP_VERSION_NOT_SUPPORTED), tokens[1]));
+         
 
 }

fea24d23025005ad0b9db3c640a551cfe4866e28
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-04-10 02:42:00 +0000

    add test.html for debugging

--- mainServ.cpp
+++ mainServ.cpp
@@ -6,18 +6,22 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 22:51:46 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/04/09 22:44:56 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/04/10 02:41:13 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "myServ.hpp"
+#include <fstream>
 
 int main(void)
 {
+   std::ofstream file("test.html");
+   
     char resp[] = "HTTP/1.1 200 OK\r\n"
     "Server: webserver-cpp\r\n"
     "Content-type: text/html\r\n\r\n"
     "<html>hello, world</html>\r\n";
+    
     
     //create poll object to handle servers
     polling pl;

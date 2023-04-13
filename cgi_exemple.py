8f4dfce8b8787081f74d2434a1196f9bf549027a
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-04-13 16:04:33 +0000

    CGI Programming using python

---
+++ cgi_exemple.py
@@ -0,0 +1,22 @@
+#!python
+
+#produce http header 
+print("Content-type:text-html; charset=UTF-8");
+print();
+
+#output the html 
+a = """
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <p>
+      this is a test
+    </p>
+</body>
+</html>
+""";
+
+print(a);
+

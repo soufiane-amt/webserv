406db114e0412146a2dc11c7da785fe20e7a02c6
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-04-13 16:58:45 +0000

    prototype of executing CGI

---
+++ cgiProgram.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cgiProgram.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 16:09:17 by fech-cha          #+#    #+#             */
+/*   Updated: 2023/04/13 16:32:57 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGIPROGRAM_HPP
+# define CGIPROGRAM_HPP
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+//prototype for executing CGI
+
+class CGI
+{
+    private:
+        std::string _name;
+        std::string _output;
+    public:
+        CGI();
+        ~CGI();
+        void    handleCGI();
+};
+
+#endif

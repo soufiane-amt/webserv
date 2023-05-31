8ff92133edbfc23044e6d6007fb8cddbea97bb3b
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-31 04:24:32 +0100

    feat: add I/O methods for client side

--- inc/appendClientSide.hpp
+++ inc/appendClientSide.hpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 20:06:16 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/05/31 02:51:43 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/05/31 03:50:21 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,15 @@ class appendClient
         appendClient();
         ~appendClient();
         
+        
+        // I/O methods for servers
+        void    sendReq(int sockfd, const void *buf, int len, int flags);
+        void    recvReq(int sockfd, void *buf, int len, int flags);
+
+        //getters & setters
         int     getClientFd(void);
         void    setClientFd(int fd);
+
 };
 
 #endif
--- srcs/socketServer/appendClientSide.cpp
+++ srcs/socketServer/appendClientSide.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 20:05:34 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/05/31 02:52:53 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/05/31 03:37:38 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,18 @@ void    appendClient::setClientFd(int fd)
 int appendClient::getClientFd(void)
 {
     return (this->_clientFd);
+}
+
+void    appendClient::sendReq(int sockfd, const void *buf, int len, int flags)
+{
+    this->sendRes = send(sockfd, buf, len, flags);
+    //close sockfd of the connection
+    close(sockfd);
+    appendClient::testSysCall(this->sendRes);
+}
+
+void    appendClient::recvReq(int sockfd, void *buf, int len, int flags)
+{
+    this->recvRes = recv(sockfd, buf, len, flags);
+    appendClient::testSysCall(this->recvRes);
 }

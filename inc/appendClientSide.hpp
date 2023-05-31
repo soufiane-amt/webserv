07f04e7e58edf1e67ff0d2456c5e50fe6d5ba648
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-31 03:13:05 +0100

    feat: working on the client class

--- inc/appendClientSide.hpp
+++ inc/appendClientSide.hpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 20:06:16 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/05/30 14:36:40 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/05/31 02:51:43 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 
 #include "/Users/fech-cha/Desktop/webserv/inc/pollingServ.hpp"
 
+
+class appendClient
+{
+    private:
+        int         _clientFd;
+        std::string _header;
+        std::string _body;
+    public:
+        appendClient();
+        ~appendClient();
+        
+        int     getClientFd(void);
+        void    setClientFd(int fd);
+};
 
 #endif

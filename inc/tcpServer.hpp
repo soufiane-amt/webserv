618e1db23ef4ed4c7108564a6300f5e1a917286c
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-06-03 06:05:33 +0100

    feat: add new checker flags

--- inc/tcpServer.hpp
+++ inc/tcpServer.hpp
@@ -6,13 +6,17 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 22:43:02 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/05/31 03:48:06 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/06/03 06:00:28 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TCPSERVER_HPP
 # define TCPSERVER_HPP
 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 #include <sys/types.h>
@@ -33,11 +37,18 @@
 
 #include "pollingServ.hpp"
 
+# define closeConnect 3
+# define responseGo 1
+# define lastChunk "0\r\n\r\n"
+# define CRLF "\r\n\r\n"
+# define endOfBody 69
+# define endOfHeader 420
+
 # define HTTP_PORT 6969
 # define BACKLOG 100
 
 //temp buff size
-# define BUFFER_SIZE 1000
+# define BUFFER_SIZE 1024
 
 class   polling;
 class tcpServer

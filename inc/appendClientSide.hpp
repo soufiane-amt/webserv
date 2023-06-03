fb26946a0700963287bfc1b1e56deed9bcefc8c0
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-06-03 06:03:55 +0100

    feat: add new methods and flags to check the client request and response status

--- inc/appendClientSide.hpp
+++ inc/appendClientSide.hpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 20:06:16 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/06/01 17:57:23 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/06/03 06:00:04 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 # define APPENDCIENTSIDE_HPP
 
 #include "/Users/fech-cha/Desktop/webserv/inc/pollingServ.hpp"
-
-# define endOfBody 69
-# define endOfHeader 420
+# define chunked 1
+# define contentlength 2 
 
 class appendClient
 {
@@ -24,6 +23,9 @@ class appendClient
         int         _checkHead;
         int         _checkBody;
         int         _clientFd;
+        int         _bodyType;
+        int         _responseStatus;
+        std::string _tmp;
         std::string _header;
         std::string _body;
     public:
@@ -33,7 +35,8 @@ class appendClient
         
         // I/O methods for servers
         void    sendReq(int sockfd);
-        void    recvReq(int sockfd);
+        void    recvHead();
+        void    recvBody(std::string req);
 
         //getters & setters
         int     getClientFd(void);
@@ -41,8 +44,21 @@ class appendClient
         
         int     getHeadStatus();
         int     getBodyStatus();
+        
         void    setHeadStatus(int head);
         void    setBodyStatus(int body);
+        
+        std::string getHeader();
+        std::string getBody();
+
+        void    setBody(std::string body);
+        
+        void                    parseChunked(std::string& chunkedData);
+        void                    copyReq(char *req, int size);
+        std::string::size_type  checkCRLForChunk(std::string test);
+        std::string             getRestOfRes(int size);
+        
+        void    getBodyRest();
 
 };
 

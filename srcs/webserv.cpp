3ea485f0df47285f90184555d84c87f9e3069cd5
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-06-07 16:35:56 +0100

    update main

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:56:03 by samajat           #+#    #+#             */
-/*   Updated: 2023/06/06 22:22:35 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/06/07 16:30:27 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,8 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include "requestParser.hpp"
-#include "responding.hpp"
-#include "errorManager.hpp"
+#include "pollingServ.hpp"
 #include <fcntl.h>
-
-
-std::string msg= "GET / HTTP/1.1\r\n"
-            "host: 192.241.213.46:6880\r\n"
-            "Upgrade-Insecure-Requests: 1\r\n"
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8\r\n"
-            "Accept-Language: en-us\r\n"
-            "Accept-Encoding: gzip, deflate\r\n"
-            "Connection: keep-alive\r\n\r\n";
-
-std::string resp = "HTTP/1.1 200 OK\r\n"
-    "Server: webserver-cpp\r\n"
-    "Content-type: text/html\r\n\r\n"
-    "<html>hello, world</html>\r\n";
     
 //According to RFC "Each header field consists of a name followed by a colon (":") and the field value. Field names are case-insensitive. "
 //So we need to convert all the keys to lower case
@@ -85,69 +68,64 @@ int	ft_parsing(int ac, char *av)
 	{
 		Config parser2(file);
 		parser = parser2;
-		parser.server_print();
+		// parser.server_print();
 	}
 	catch(std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
+		return (-1);
 	}
 	return (0);
 }
 
 
-std::vector<char>    request_response(std::string msg)
+int main (int argc, char **argv, char **env)
 {
-    clientRequestParser test(msg);
-    http_message_t &_request = test.getRequest();
-
-    try
+	(void)env;
+	//create poll object to handle servers
+    polling pl;
+	
+	if (ft_parsing(argc, argv[1]) < 0)
+		return (1);
+    if (argc != 2)
     {
-            errorManager::isRequestValid(_request);
-            responsePreparation response(_request);
-        
-            return response.get_response();
-        
+        std::cout << "Usage: ./webserv [port]" << std::endl;
+        return 0;
     }
-    catch(const StatusCode& e)
-    {
-        responsePreparation response(_request, e);
-        return response.get_response();
-    }
-    return std::vector<char>();
-}
+	std::cout << "Welcome To Webserv1.0: " << std::endl;
+    std::multimap<std::string,std::string> servs = parser.get_host();
+	std::multimap<std::string, std::string>::iterator it = servs.begin();
+	std::cout << "Size = " << servs.size() << std::endl;
+	for (;it != servs.end(); it++)
+	{
+		std::istringstream iss(it->first);
+		int port;
+		iss >> port;
+		std::cout << port << std::endl;
+		std::cout << it->second << std::endl;
+		tcpServer sock(pl, port, it->second);
+		pl.pushServer(sock);
+	}
 
-// int main (int argc, char **argv)
-// {
-// 	if (ft_parsing(argc, argv[1]))
-// 		return (1);
-//     if (argc != 2)
-//     {
-//         std::cout << "Usage: ./webserv [port]" << std::endl;
-//         return 0;
-//     }
-//     try
-//     {
-// 		int port;
-// 		std::cin >> port;
-//         tempServer(port);
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
+	 while (1)
+    {
+        //waiting for events using poll()
+        int poll_count = pl.callPoll((pollfd *)pl.getPollfdAddrs(), pl.getSize(), -1);
+        
+        if (poll_count  == -1)
+        {
+            perror("poll");
+            // exit(EXIT_FAILURE);
+            continue;
+        }
+        else
+        {
+            //handle I/O events
+            pl.handlePoll();        
+        }
+    }
     
-    // clientRequestParser test(msg);
-    // http_message_t &_request = test.getRequest();
-
-    // try
-    // {
-    //     errorManager::isRequestValid(_request);
-    //     responsePreparation response(_request);
-    //     std::cout << response.get_response();
-    // }
-    // catch(const StatusCode& e)
-    // {
-    //     responsePreparation response(_request, e);
-    //     std::cout << response.get_response();
-    // }
-// }
+    //calling destructor will close all connections
+    return (0);
+   
+}

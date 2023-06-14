7b679031138a6a1516aa4121ae5ec6620e55143d
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-06-14 14:06:00 +0100

    feat: upload form and python script

---
+++ srcs/upload/upload.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+
+import cgi
+import os
+
+form = cgi.FieldStorage()
+
+if 'file' in form:
+    # Get the uploaded file
+    file_item = form['file']
+    upload_dir = os.environ['UPLOAD_DIR']
+
+    # Check if the file was uploaded
+    if file_item.filename:
+        # Open a file for writing
+        file = open(upload_dir + file_item.filename, 'wb')
+
+        # Write the file to the server
+        file.write(file_item.file.read())
+        file.close()
+        message = 'File Uploaded Successfully'
+    else:
+        message = "Error uploading file";
+else:
+    message = 'No file was uploaded'
+
+print("Content-type: text/html\n")
+print("<html>")
+print("<body>")
+print("<p>%s</p>" % message)
+print("</body>")
+print("</html>")
